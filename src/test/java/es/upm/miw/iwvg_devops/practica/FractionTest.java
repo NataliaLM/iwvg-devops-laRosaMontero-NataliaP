90e1f462279338f805d6282817661d31e0538100
NataliaLM <natalia.arq.123@gmail.com>
2023-09-28 19:57:51 +0200

    add Fraction Test with functionalities #9

--- src/test/java/es/upm/miw/iwvg_devops/practica/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/practica/FractionTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class FractionTest {
     private Fraction fraction;
-
+    private Fraction anotherFraction;
     @BeforeEach
-    public void setUp() {
+    public void before() {
+
         fraction = new Fraction(5, 6);
+        anotherFraction = new Fraction(3, 4);
     }
 
     @Test
@@ -46,4 +48,42 @@ public class FractionTest {
     public void testToString() {
         assertEquals("Fraction{numerator=5, denominator=6}", fraction.toString());
     }
+
+    @Test
+    public void testIsProper() {
+        assertTrue(fraction.isProper());
+        assertFalse(anotherFraction.isProper());
+    }
+
+    @Test
+    public void testIsImproper() {
+        assertFalse(fraction.isImproper());
+        assertTrue(anotherFraction.isImproper());
+    }
+
+    @Test
+    public void testIsEquivalent() {
+        Fraction equivalentFraction = new Fraction(15, 24);
+        assertTrue(fraction.isEquivalent(equivalentFraction));
+        assertFalse(fraction.isEquivalent(anotherFraction));
+    }
+
+    @Test
+    public void testAdd() {
+        Fraction result = fraction.add(anotherFraction);
+        assertEquals(new Fraction(17, 12), result);
+    }
+
+    @Test
+    public void testMultiply() {
+        Fraction result = fraction.multiply(anotherFraction);
+        assertEquals(new Fraction(1, 2), result);
+    }
+
+    @Test
+    public void testDivide() {
+        Fraction result = fraction.divide(anotherFraction);
+        assertEquals(new Fraction(8, 9), result);
+    }
+
 }
